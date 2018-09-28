e8247bc49ceb13f20282b0406667ae2bca6f0f18
Max <poeschl.max@gmail.com>
2018-09-28 22:11:40 +0200

    Add multipages

--- webapp/app.py
+++ webapp/app.py
@@ -20,8 +20,14 @@ df['datetime'] = pd.to_datetime(df.datetime)
 
 # ECU Gadget imports end
 external_stylesheets = ['static/style.css']
-
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app.config.suppress_callback_exceptions = True
+
+app.layout = html.Div([
+    dcc.Location(id='url', refresh=False),
+    html.Div(id='page-content')
+])
+
 ecu_temperature_gadget = ec.EcuTemperatureGadget()
 
 STATIC_PATH = os.path.join(os.path.dirname(
@@ -32,8 +38,7 @@ STATIC_PATH = os.path.join(os.path.dirname(
 def serve_static(resource):
     return flask.send_from_directory(STATIC_PATH, resource)
 
-
-app.layout = html.Div(children=[
+index_page = html.Div(children=[
     html.Meta(name='viewport', content='width=device-width, initial-scale=1.0'),
     html.Link(rel='shortcut icon', href='static/favicon.ico'),
 
@@ -69,10 +74,12 @@ app.layout = html.Div(children=[
 
     html.Div([
         html.Div([
-            html.Div(
+            dcc.Link(
+                html.Div(
                 #need to pass the start and end from the slider callback
                 ehc.get_pie_chart(ecu_temperature_gadget),
-                className='test'),
+                className='test')
+                , href='/ecu'),
         ], className='charts'),
         html.Div([
             html.Div(
@@ -87,6 +94,94 @@ app.layout = html.Div(children=[
     ], className='main'),
 
 ])
+
+turbo = html.Div([
+        html.Meta(name='viewport', content='width=device-width, initial-scale=1.0'),
+    html.Link(rel='shortcut icon', href='static/favicon.ico'),
+
+    html.Div([
+        html.Img(src='static/mechPandaProfile.png',
+                 height='40', width='40', className='accountPic'),
+        html.P('Mechanic Panda', className='accountName')
+    ], className="account"),
+
+    html.Div([
+        html.Div([
+            html.P('Time Range'),
+            dcc.RangeSlider(
+                count=1,
+                min=-60,
+                max=0,
+                step=1.0,
+                marks={
+                    -60: '-60 Days',
+                    -40: '-40 Days',
+                    -20: '-20 Days',
+                    0: 'today'
+                },
+                value=[-5, 0]
+            )], className='slider'),
+        html.Div(
+            dcc.Input(
+                placeholder='Search (VIN, Brand, etc.)',
+                type='text',
+                value=''
+            ), className='search'),
+    ], className='header'),
+
+    html.Div([html.P('Turbo drilldown'),
+        # Put in Turbo drilldown code here
+    ], className='main'),
+])
+
+ecu = html.Div([
+        html.Meta(name='viewport', content='width=device-width, initial-scale=1.0'),
+    html.Link(rel='shortcut icon', href='static/favicon.ico'),
+
+    html.Div([
+        html.Img(src='static/mechPandaProfile.png',
+                 height='40', width='40', className='accountPic'),
+        html.P('Mechanic Panda', className='accountName')
+    ], className="account"),
+
+    html.Div([
+        html.Div([
+            html.P('Time Range'),
+            dcc.RangeSlider(
+                count=1,
+                min=-60,
+                max=0,
+                step=1.0,
+                marks={
+                    -60: '-60 Days',
+                    -40: '-40 Days',
+                    -20: '-20 Days',
+                    0: 'today'
+                },
+                value=[-5, 0]
+            )], className='slider'),
+        html.Div(
+            dcc.Input(
+                placeholder='Search (VIN, Brand, etc.)',
+                type='text',
+                value=''
+            ), className='search'),
+    ], className='header'),
+
+    html.Div([html.P('ECU drilldown'),
+        # Put in ECU drilldown code here
+    ], className='main'),
+])
+
+@app.callback(dash.dependencies.Output('page-content', 'children'),
+    [dash.dependencies.Input('url', 'pathname')])
+def display_page(pathname):
+    if pathname == '/ecu':
+        return ecu
+    elif pathname == '/turbo':
+        return turbo
+    else:
+        return index_page
 
 if __name__ == '__main__':
     app.run_server(debug=True)
